3214063a49adca8d51f28c3fea940fbc03589aa3
jvpoulos <jvpoulos@gmail.com>
2016-05-29 13:33:54 -0400

    clean up slave pums

--- code/ipums-1860.R
+++ code/ipums-1860.R
@@ -45,39 +45,3 @@ slave.60 <- subset(slave.60[!duplicated(slave.60$id),], select=c("id","county","
 
 # Estimate avg. values of slaves in 1860
 slave.60$slave.value <- slave.60$n.slaves * 778
-
-### Link 1860 Census 100% sample to slave schedule ###
-
-# link.vars <- c("surname", "first", "state")
-# 
-# r.pairs.slave <- RLBigDataLinkage(ipums.60[ipums.60$state!="North Carolina",][link.vars],
-#                                  slave.60[link.vars], 
-#                                  blockfld = list(3),
-#                                  strcmp = c(1:2), 
-#                                  strcmpfun = "jarowinkler")
-# 
-# saveRDS(r.pairs.slave, paste0(data.directory,"r.pairs.slave.rds"))
-# r.pairs.slave <- readRDS(paste0(data.directory,"r.pairs.slave.rds"))
-# 
-# min.train.slave <- getMinimalTrain(r.pairs.slave,nEx=1) 
-# min.train.slave$pairs$is_match <- 0
-# min.train.slave$pairs$is_match[min.train.slave$pairs$surname>0.90 & 
-#                                  min.train.slave$pairs$first>0.7 &
-#                                  min.train.slave$pairs$state==1] <-1 
-# 
-# sum(min.train.slave$pairs$is_match)
-# 
-# model.slave <- trainSupv(min.train.slave, method = "svm")
-# result.slave <- classifySupv(model.slave, newdata = r.pairs.slave)
-# summary(result.slave)
-# 
-# links.slave <- result.slave$pairs[result.slave$prediction=="L",][c("id1","id2")]
-# links.slave <- links.slave[!duplicated(links.slave$id1),] # remove duplicates
-# 
-# ipums.60 <- merge(ipums.60, links, by.x="did",by.y="id1", all.x=TRUE)
-# ipums.60 <- merge(ipums.60, slave.60, by.x="pid",by.y="id", all.x=TRUE)
-# colnames(ipums.60)[1] <- "vid"
-
-### Link 1860 Census 1% sample to 100% sample + slaves ###
-
-### Predict 1860 taxable property in 100% file ###
